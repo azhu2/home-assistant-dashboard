0bd2422519df153e8179b3cda05ab2fd01c26806
Alex Zhu <alex@alexazhu.com>
2023-02-03 02:10:08 -0800

    typo

--- src/mappers/renamed-entities.ts
+++ src/mappers/renamed-entities.ts
@@ -5,7 +5,7 @@ const renamedEntityMap: {[key: string]: string} = {
     'sensor.udr_memory_utilization': 'UDR RAM',
     'sensor.synology_nas_volume_1_volume_used': 'NAS Storage',
     'sensor.udr_storage_utilization': 'UDR Storage',
-    'sensor.1m_download_max': 'Downnload',
+    'sensor.1m_download_max': 'Download',
     'sensor.1m_upload_max': 'Upload',
     'sensor.nest_temperature_sensor_family_room_temperature': 'Temp',
     'sensor.master_bedroom_temperature_sensor_temperature': 'Temp',
