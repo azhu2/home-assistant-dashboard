b4b5bc6683f4af52282c654ed10115be17c867a8
Alex Zhu <alex@alexazhu.com>
2023-11-19 23:48:52 -0800

    Reorganize gauge directory

---
+++ src/components/tiles/gauge/needle-gauge.tsx
@@ -0,0 +1,55 @@
+import * as color from '../../../types/color';
+import { Gauge } from './gauge';
+
+
+export class NeedleGauge extends Gauge {
+    render() {
+        let background;
+        if (typeof this.props.state === 'number' &&
+            typeof this.props.min === 'number' &&
+            typeof this.props.max === 'number') {
+            const pct = (this.props.state - this.props.min) / (this.props.max - this.props.min);
+
+            /*
+             * Green -> Yellow -> Red scaling scheme
+             * pct 0.0 -> 0.5 -> 1.0
+             * r   0   -> 255 -> 255
+             * g   255 -> 255 -> 0
+             */
+            const fillColor = new color.Color(
+                Math.min(255, 256 * Math.min(1, (pct * 2))),
+                Math.min(255, 256 * Math.min(1, (2 - pct * 2))),
+                0,
+                64);
+
+            background =
+                <svg viewBox='0 0 1 0.5'>
+                    {/* Base circle */}
+                    <circle cx='0.5' cy='0.5' r='0.5' fill={fillColor.rgbString(true)} />
+                    {/* Inner circle */}
+                    <circle cx='0.5' cy='0.5' r='0.3' fill='white' />
+                    {/* Cover unused part of dial with rotated rectangle */}
+                    <rect
+                        x='0' y='0.5' width='1.5' height='0.5' fill='white'
+                        transform={`rotate(-${Math.round((1 - pct) * 180)})`}
+                        transform-box='view-box' transform-origin='bottom'
+                    />
+                    {/* Needle */}
+                    <path d='M0 0.5 L0.46 0.48 Q0.5 0.5 0.46 0.52 Z' fill='black'
+                        transform={`rotate(${Math.round(pct * 180)})`}
+                        transform-box='view-box' transform-origin='bottom'
+                    />
+                </svg>;
+        }
+
+        return this.renderHelper(background);
+    }
+}
+
+export class PercentGauge extends Gauge {
+    render() {
+        return (
+            <NeedleGauge {...this.props} min={0} max={100} />
+        )
+    }
+}
---
+++ src/components/tiles/gauge/temp-sensor-gauge.tsx
@@ -0,0 +1,34 @@
+import * as haEntity from '../../../types/ha-entity';
+import * as tile from '../../tile/tile';
+import { Icon } from '../../icon/icon';
+import { Gauge } from './gauge';
+import * as gauge from './gauge';
+import { HistoryGauge } from './history-gauge';
+
+/** A HistoryGauge with an icon to indicate room occupancy */
+export class TempSensorGauge extends Gauge {
+    propsMapper(entity: haEntity.Entity, options: tile.Options): tile.MappedProps<gauge.Props> {
+        const gaugeProps = super.propsMapper(entity, options);
+
+        let toggleIcon;
+        if (options.secondaryEntities && options.secondaryEntities.length > 0) {
+            let iconProps = {
+                name: 'user-male-circle',
+                color: '6644aa',
+            };
+            toggleIcon = {
+                entity: options.secondaryEntities[0],
+                onIcon: <Icon {...iconProps} filled={true} />,
+                offIcon: <Icon {...iconProps} filled={false} />,
+            }
+        }
+
+        return {...gaugeProps, toggleIcon};
+    }
+
+    render() {
+        return (
+            <HistoryGauge {...this.props} />
+        )
+    }
+}
