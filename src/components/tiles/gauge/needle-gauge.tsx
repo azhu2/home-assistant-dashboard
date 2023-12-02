d629d0afffc4a20d6a30800698366aee7e8d0113
Alex Zhu <alex@alexazhu.com>
2023-12-02 03:53:56 -0800

    Add background to needle gauge

--- src/components/tiles/gauge/needle-gauge.tsx
+++ src/components/tiles/gauge/needle-gauge.tsx
@@ -20,26 +20,46 @@ export class NeedleGauge extends Gauge {
                 255 * Math.min(1, (pct * 2)),
                 255 * Math.min(1, 2 - pct * 2),
                 0,
-                64);
+                96);
+            const rotateDeg = Math.round(pct * 180);
+            const positiveClipPathId = `show-clip-${this.props.entityID.getCanonicalized()}`;
+            const negativeClipPathId = `hide-clip-${this.props.entityID.getCanonicalized()}`;
 
             background =
-                <svg viewBox='0 0 1 0.5' preserveAspectRatio='xMidYMax meet'>
-                    {/* Base circle */}
-                    <circle cx='0.5' cy='0.5' r='0.5' fill={fillColor.rgbString(true)} />
-                    {/* Inner circle */}
-                    <circle cx='0.5' cy='0.5' r='0.3' fill='white' />
-                    {/* Cover unused part of dial with rotated rectangle */}
-                    <rect
-                        x='0' y='0.5' width='1.5' height='0.5' fill='white'
-                        transform={`rotate(-${Math.round((1 - pct) * 180)})`}
-                        transform-box='view-box' transform-origin='bottom'
-                    />
-                    {/* Needle */}
-                    <path d='M0 0.5 L0.46 0.48 Q0.5 0.5 0.46 0.52 Z' fill='black'
-                        transform={`rotate(${Math.round(pct * 180)})`}
-                        transform-box='view-box' transform-origin='bottom'
-                    />
-                </svg>;
+                <>
+                    <svg viewBox='0 0 1 0.5' preserveAspectRatio='xMidYMax meet'>
+                        <defs>
+                            <clipPath id={positiveClipPathId}>
+                                <rect
+                                    x={0} y={0.5}
+                                    width={1} height={0.5}
+                                    transform={`rotate(${rotateDeg})`}
+                                    transform-box='view-box' transform-origin='bottom'
+                                />
+                            </clipPath>
+                            <clipPath id={negativeClipPathId}>
+                                <rect
+                                    x={0} y={0.5}
+                                    width={1} height={0.5}
+                                    transform={`rotate(${rotateDeg + 180})`}
+                                    transform-box='view-box' transform-origin='bottom'
+                                />
+                            </clipPath>
+                        </defs>
+
+                        {/* Base circle */}
+                        <circle cx='0.5' cy='0.5' r='0.5' fill={fillColor.rgbString(true)} clipPath={`url(#${positiveClipPathId}`} />
+                        {/* Background circle */}
+                        <circle cx='0.5' cy='0.5' r='0.5' fill='#eeeeee' clipPath={`url(#${negativeClipPathId}`} />
+                        {/* Inner circle */}
+                        <circle cx='0.5' cy='0.5' r='0.3' fill='white' />
+                        {/* Needle */}
+                        <path d='M0 0.5 L0.46 0.48 Q0.5 0.5 0.46 0.52 Z' fill='black'
+                            transform={`rotate(${rotateDeg})`}
+                            transform-box='view-box' transform-origin='bottom'
+                        />
+                    </svg>
+                </>;
         }
 
         return this.renderHelper(background);
